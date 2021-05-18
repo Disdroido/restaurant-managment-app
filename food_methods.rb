e6cfb6a148f66b74c46c28d39c5c47aae983c8c0
disdroido <disdroido@gmail.com>
2021-05-18 12:35:20 +1000

    fixed adding data so it doesn't remove all data in JSON file

--- food_methods.rb
+++ food_methods.rb
@@ -8,35 +8,45 @@ puts @data_hash
 
 class FoodMethods
     def add_food
-        # items = []
-        # getAllUserInput = false
-        # until getAllUserInput do
-        #     newfood = {}
-        #     puts "New food name"
-        #     newfood["name"] = gets.chomp
-        #     puts "Price for new food"
-        #     newfood["price"] = gets.chomp
+        file = File.read('menu/menu2.json')
+        @data_hash = JSON.parse(file)
+        getAllUserInput = false
+        until getAllUserInput do
+            newfood = {}
+            puts "New food name"
+            newfood["name"] = gets.chomp
+            puts "Price for new food"
+            newfood["price"] = gets.chomp
 
-        #     @data_hash.merge(newfood)
-    
-        #     items << newfood
+            @data_hash << newfood
+            puts @data_hash
             
-        #     puts "Would you like to add more food?"
-        #     verifystep = gets.chomp
-        #     if verifystep.downcase[0] == "n"
-        #         getAllUserInput = true
-        #     end
-        # end
-        # push = JSON.load(items)
-        # File.write('menu/menu2.json', JSON.dump(push))
+            puts "Would you like to add more food?"
+            verifystep = gets.chomp
+            if verifystep.downcase[0] == "n"
+                getAllUserInput = true
+            end
+        end
+        File.write('menu/menu2.json', JSON.dump(@data_hash))
     end
 
     def delete_food
+        file = File.read('menu/menu2.json')
+        @data_hash = JSON.parse(file)
+
         puts "These are all of the items you can delete: \n#{@data_hash}"
 
+        puts "Type the name of the food you want to delete"
         foodName = gets.chomp
 
         @data_hash.delete_if {|element| element["name"] == foodName}
         File.write('menu/menu2.json', JSON.dump(@data_hash))
     end    
+
+    def show_menu
+        file = File.read('menu/menu2.json')
+        @data_hash = JSON.parse(file)
+
+        puts @data_hash
+    end
 end
--- index.rb
+++ index.rb
@@ -1,15 +1,17 @@
 require_relative "food_methods.rb"
 foodMethods = FoodMethods.new
 
-puts "What would you like to do:\n1. Add new food items\n2. Delete food items"
+puts "What would you like to do:\n1. Add new food items\n2. Delete food items\n3. Show menu"
 
 userInput = gets.chomp.to_i
 
 case userInput
 when userInput = 1
     foodMethods.add_food
-when
+when userInput = 2
     foodMethods.delete_food
+when userInput = 3
+    foodMethods.show_menu
 else
 
 end
